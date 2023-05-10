2ad9791037c3928545a60f2efd35502328a04627
Jokeslayer <aschwartz07901@gmail.com>
2023-05-09 20:00:52 -0600

    progress

--- js/main.js
+++ js/main.js
@@ -14,11 +14,7 @@ const COLORS = {
     '8': 'grey'
 };
 
-const CELL_STATE = {
-    '0' : '',
-    '-1': 'bomb',
-    '1' : 'bomb-adj'
-};
+
 
 const DIF_SET = {
     '0': [9,9,10],
@@ -40,10 +36,26 @@ class Cell{
         this.isFlagged = false;
         this.adjMineCount = 0;
     }
+/* 
+    createAdjNumbers(){
 
-    countAdjacent(){
-        
+        this.adjMineCount = this.adjMineCount + checkNeighbor(this.row++,this.col);
+        console.log(this.adjMineCount)
+        this.adjMineCount = this.adjMineCount + checkNeighbor(this.row++,this.col--);
+        this.adjMineCount = this.adjMineCount + checkNeighbor(this.row++,this.col++);
+        this.adjMineCount = this.adjMineCount + checkNeighbor(this.row--,this.col);
+        this.adjMineCount = this.adjMineCount + checkNeighbor(this.row--,this.col--);
+        this.adjMineCount = this.adjMineCount + checkNeighbor(this.row--,this.col++);
+        this.adjMineCount = this.adjMineCount + checkNeighbor(this.row,this.col--);
+        this.adjMineCount = this.adjMineCount + checkNeighbor(this.row,this.col++);
     }
+
+    checkNeighbor(r,c){
+        if(r < 0 || r >= board.length || c < 0 || c >= board[r].length) return 0;
+        if(board[r][c].isMine) return 1;
+        return 0;
+    } */
+    
 
     clear(){
 
@@ -57,6 +69,7 @@ let board=[];
 let difficulty;           //determines the size of the game board
 let winner;        // null = no winner, 1 or -1 winner, T = tied game
 let gameOver;     // boolean which determines whether a bomb has been clicked on
+
 
 /*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 CACHED ELEMENTS
@@ -82,65 +95,62 @@ document.getElementById('board').addEventListener('click',handleChoice);
 document.getElementById('board').addEventListener('contextmenu',flag);
 document.getElementById('board').addEventListener('mousedown',handleSweep);
 
-playAgainBtnEl.addEventListener('click',reset);
-newGameBtnEl.addEventListener('click',reset);
+playAgainBtnEl.addEventListener('click',init);
+newGameBtnEl.addEventListener('click',init);
 difficultyBtnEl.addEventListener('click',toggle_game_menu);
 instructionBtnEl.addEventListener('click',toggle_instructions);
 
 /*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 FUNCTIONS
 $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
-reset();
+init();
 
 // Initialize state and call render
 function init() {
-    
+    console.log('push the button')
+    timerEl.innerHTML='';
+    boardEl.innerHTML = '';
+    board = [];
+    difficulty = document.querySelector('input[name="difficulty"]:checked').value;
+    if(difficulty === "custom"){
+        let boardSize = customRow.value*customHeight.value;
+        const mines = generateMines()
+        createBoard(customRow.value,customHeight.value,customMines.value);
+
+    }
+    else{
+        createBoard(DIF_SET[difficulty][0],DIF_SET[difficulty][1],DIF_SET[difficulty][2]);
+    }    
     winner = null;
     gameOver = false;
     render();
 }
 
 function getWinner(){
+    for(let r=0;r<board.length;r++){
+        for(let c=0;c<board[r].length;c++){
+            if(board[r][c].isMine) continue;
+            if(board[r][c].isRevealed) continue;
+            if(board[r][c].isRevealed===false)return false;
+        }
+    }
+    return true
 }
+
 
 //digitalize state
 function render(){
     renderBoard();
 }
 
-function renderBoard(){
-    for(let r=0;r<board.length;r++){
-        
-        for(let c=0;c<board[r].length;c++){
-            
-        }
-    }
-}
-
-function renderMessage(){
-}
-
-function renderControls(){
-}
-
-
-function createBoard(row,col,mines){
-    
-    let boardSize = row*col;
-    const coordinates = generateMines(mines, boardSize);
-    createStatus(mines);
-    createCells(row,col,coordinates);
-    createAdjNumbers();
-}
-
-function createCells(row,col,arr){
+function renderBoard(arr){
     count =1;
-    for(let r=0;r<(row);r++){
+    for(let r=0;r<row;r++){
         board[r]=[];
         let row_index = document.createElement('tr');
         boardEl.appendChild(row_index);
         row_index.id=`r${r}`;
-        for(let c=0;c<(col);c++){
+        for(let c=0;c<col;c++){
             board[r][c]=new Cell(r,c,count);
             let cell=document.createElement('td');
             cell.id=`r${r}c${c}`;
@@ -149,7 +159,6 @@ function createCells(row,col,arr){
                 board[r][c].isMine=true;
                 let content = document.createElement('img');
                 content.setAttribute("src", "AV/bomb.png");
-                
                 cell.appendChild(content);
             }
             row_index.appendChild(cell);
@@ -158,22 +167,57 @@ function createCells(row,col,arr){
     }
 }
 
-function createAdjNumbers(){
-    for(let r=0;r<board.length;r++){
-        for(let c=0;c<board[r].length;c++){
-            if(board[r][c].isMine) continue;
-            else{
-                
+function renderMessage(){
+}
+
+
+function createBoard(row,col,mines){
+    
+    
+    createStatus(mines);
+    createCells(row,col,coordinates);
+    //populateAdjNumbers(row,col);
+}
+
+function createCells(row,col,arr){
+    count =1;
+    for(let r=0;r<row;r++){
+        board[r]=[];
+ 
+        boardEl.appendChild(row_index);
+        row_index.id=`r${r}`;
+        for(let c=0;c<col;c++){
+            board[r][c]=new Cell(r,c,count);
+            let cell=document.createElement('td');
+            cell.id=`r${r}c${c}`;
+            cell.classList.add('tile');
+            if(arr.includes(count)){
+                board[r][c].isMine=true;
+                let content = document.createElement('img');
+                content.setAttribute("src", "AV/bomb.png");
+                cell.appendChild(content);
             }
+            row_index.appendChild(cell);
+            count++;
         }
     }
 }
 
+/* function populateAdjNumbers(row, col){
+    for(let r=0;r<board.length;r++){
+        for(let c=0;c<board[r].length;c++){
+            board[r][c].createAdjNumbers();
+            console.log(board[r][c].adjMineCount);
+        }
+    }
+} */
+
 function handleChoice(event){
+    
     let index = event.target.id;
     let loc = getCoordinates(index);
-    console.log(index)
-    if(board[loc[0]][loc[1]].isMine){
+    console.log(board[loc[0]][loc[1]].ro)
+    if(board[loc[0]][loc[1]]){
         gameOver=true;
     }
     else if(board[loc[0]][loc[1]].isRevealed===false){
@@ -210,22 +254,10 @@ function toggle_instructions(event){
     }
 }
 
-function reset(){
-    boardEl.innerHTML = '';
-    board = [];
-    difficulty = document.querySelector('input[name="difficulty"]:checked').value;
-    if(difficulty === "custom"){
-        createBoard(customRow.value,customHeight.value,customMines.value);
-    }
-    else{
-        createBoard(DIF_SET[difficulty][0],DIF_SET[difficulty][1],DIF_SET[difficulty][2]);
-    }
-}
-
-  function generateMines(total, size){
+function generateMines(total, boardSize){
     let rands = [];
     while(rands.length < total){
-        const r = Math.floor(Math.random() * size);
+        const r = Math.floor(Math.random() * boardSize);
         if(rands.indexOf(r)!==-1){
             continue;
         }
@@ -239,6 +271,7 @@ function reset(){
 function createStatus(mines){
     mineCounterEl.innerText = mines;
     let elapsedTime = 0;
+    timerEl.innerHTML='';
     timerEl.innerHTML = elapsedTime;
     const timerId = setInterval(function() {
       if (elapsedTime < 1000){
@@ -250,11 +283,12 @@ function createStatus(mines){
           clearInterval(timerId);  // BUG FIX
         }
      }, 1000);
-  }
+}
 
-  function getCoordinates(target){
+function getCoordinates(target){
     let chop = target.split('');
     let result = [chop[1],chop[3]];
     result = result.map(Number);
     return result
-  }
+}
+
